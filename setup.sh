ce514e290733184438b71a3cf3fbc9789f2a83e5
sheepymeh <sheepymeh@users.noreply.github.com>
2019-12-30 22:27:48 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,8 @@
-sudo systemctl enable NetworkManager
-sudo systemctl startNetworkManager
-
-sudo pacman -R --noconfirm vi memtest86+ spectre-meltdown-checker tlp zsh f2fs-tools
-
-rm -rf .config/i3-scrot.conf .config/Ranger
-
-sudo pacman -S --noconfirm yay
-
-sudo yay -S --noconfirm android-tools blueman code firefox gcc libmpc php-fpm nginx-mainline nodejs npm i3blocks lollypop youtube-dl arc-gtk-theme papirus-icon-theme throttled noto-fonts-sc ttf-roboto ttf-roboto-mono ttf-font-awesome postman-bin lightdm-mini-greeter slurp grim wl-clipboard termite thunar-archive-plugin thunar wofi-hg xorg-server xorg-server-xwayland xorg-xrandr p7zip unzip mako
+sudo yay -S --noconfirm android-tools blueman code firefox gcc libmpc php-fpm nginx-mainline nodejs npm i3blocks lollypop youtube-dl arc-gtk-theme papirus-icon-theme throttled noto-fonts-sc ttf-roboto ttf-roboto-mono ttf-font-awesome postman-bin lightdm-mini-greeter slurp grim wl-clipboard termite thunar-archive-plugin thunar wofi-hg xorg-server xorg-server-xwayland xorg-xrandr p7zip unzip mako sway
 # mongodb?
 
-sudo systemctl enable bluetooth
 sudo systemctl enable lenovo_fix
+sudo systemctl enable lightdm
 
 git config --global user.name 'sheepymeh'
 git config --global user.email 'sheepymeh@users.noreply.github.com'
@@ -20,13 +11,14 @@ git config --global credential.helper store
 
 chmod a+x battery.sh
 sudo mv battery.sh /usr/local/bin/i3blocks/battery.sh
-mv sway.conf ~/$XDG_CONFIG_HOME/sawy/config
-mv wofi.conf ~/$XDG_CONFIG_HOME/wofi/config
-mv wofi.css ~/$XDG_CONFIG_HOME/wofi/style.css
-mv termite.conf ~/$XDG_CONFIG_HOME/termite/config
-mv i3blocks.conf ~/$XDG_CONFIG_HOME/i3blocks/config
-mv mako.conf ~/$XDG_CONFIG_HOME/mako/config
+sudo mkdir -p ~/.config/sway ~/.config/wofi ~/.config/termite ~/.config/mako ~/.config/i3blocks
+mv sway.conf ~/.config/sawy/config
+mv wofi.conf ~/.config/wofi/config
+mv wofi.css ~/.config/wofi/style.css
+mv termite.conf ~/.config/termite/config
+mv i3blocks.conf ~/.config/i3blocks/config
+mv mako.conf ~/.config/mako/config
 sudo mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.old.conf
 sudo mv lightdm.conf /etc/lightdm/lightdm.conf
-sudo mv lightdm-mini-greeter.conf /etc/lightdm/lightdm-mini-greeter.old.conf
+sudo mv /etc/lightdm/lightdm-mini-greeter.conf /etc/lightdm/lightdm-mini-greeter.old.conf
 sudo mv lightdm-mini-greeter.conf /etc/lightdm/lightdm-mini-greeter.conf
