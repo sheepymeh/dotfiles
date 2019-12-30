99356f77d5e9fd60cc46eccb22df71547f4daa81
sheepymeh <sheepymeh@users.noreply.github.com>
2019-12-30 17:14:17 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
+sudo systemctl enable NetworkManager
+sudo systemctl startNetworkManager
+
 sudo pacman -R --noconfirm vi memtest86+ spectre-meltdown-checker tlp zsh f2fs-tools
 
 rm -rf .config/i3-scrot.conf .config/Ranger
 
 sudo pacman -S --noconfirm yay
 
-sudo yay -S --noconfirm android-tools blueman code firefox gcc libmpc php-fpm nginx-mainline nodejs npm i3blocks lollypop youtube-dl arc-gtk-theme papirus-icon-theme throttled noto-fonts-sc ttf-roboto ttf-roboto-mono ttf-font-awesome postman-bin lightdm-mini-greeter slurp grim wl-clipboard termite thunar-archive-plugin thunar wofi-hg xorg-server xorg-server-xwayland xorg-xrandr p7zip unzip
+sudo yay -S --noconfirm android-tools blueman code firefox gcc libmpc php-fpm nginx-mainline nodejs npm i3blocks lollypop youtube-dl arc-gtk-theme papirus-icon-theme throttled noto-fonts-sc ttf-roboto ttf-roboto-mono ttf-font-awesome postman-bin lightdm-mini-greeter slurp grim wl-clipboard termite thunar-archive-plugin thunar wofi-hg xorg-server xorg-server-xwayland xorg-xrandr p7zip unzip mako
 # mongodb?
 
 sudo systemctl enable bluetooth
@@ -22,6 +25,7 @@ mv wofi.conf ~/$XDG_CONFIG_HOME/wofi/config
 mv wofi.css ~/$XDG_CONFIG_HOME/wofi/style.css
 mv termite.conf ~/$XDG_CONFIG_HOME/termite/config
 mv i3blocks.conf ~/$XDG_CONFIG_HOME/i3blocks/config
+mv mako.conf ~/$XDG_CONFIG_HOME/mako/config
 sudo mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.old.conf
 sudo mv lightdm.conf /etc/lightdm/lightdm.conf
 sudo mv lightdm-mini-greeter.conf /etc/lightdm/lightdm-mini-greeter.old.conf
