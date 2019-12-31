b489cb39b4cb3ffaba48b6970fa5c35c0a48694d
sheepymeh <sheepymeh@users.noreply.github.com>
2019-12-31 15:41:54 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-sudo pacman -S --noconfirm yay
-yay -S --noconfirm acpi alsa-utils android-tools arc-gtk-theme base bash-completion brightnessctl code cups-pdf dialog dmidecode efibootmgr exfat-utils firefox grim grub gvfs i3blocks intel-ucode light lightdm-mini-greeter linux linux-firmware lollypop lsof mako nano neofetch networkmanager nginx-mainline noto-fonts-sc npm ntfs-3g p7zip pacman-contrib papirus-icon-theme php-fpm plymouth plymouth-theme-arch-agua postman-bin pulseaudio-alsa pulseaudio-bluetooth qbittorrent slurp sway termite throttled thunar-archive-plugin thunar-volman ttf-font-awesome ttf-roboto ttf-roboto-mono unzip wget wl-clipboard wob wofi-hg xbindkeys xdg-user-dirs xf86-video-intel xf86-video-nouveau xorg-server xorg-server-xwayland xorg-xrandr yay youtube-dl
+yay -S --noconfirm acpi alsa-utils android-tools arc-gtk-theme base bash-completion brightnessctl code cups-pdf dialog dmidecode efibootmgr exfat-utils firefox grim grub gvfs i3blocks intel-ucode light lightdm lightdm-mini-greeter linux linux-firmware lollypop lsof mako nano neofetch networkmanager nginx-mainline noto-fonts-sc npm ntfs-3g p7zip pacman-contrib papirus-icon-theme php-fpm plymouth plymouth-theme-arch-agua postman-bin pulseaudio-alsa pulseaudio-bluetooth qbittorrent slurp sway termite throttled thunar-archive-plugin thunar-volman ttf-font-awesome ttf-roboto ttf-roboto-mono unzip wget wl-clipboard wob wofi-hg xbindkeys xdg-user-dirs xf86-video-intel xf86-video-nouveau xorg-server xorg-server-xwayland xorg-xrandr yay youtube-dl
 # mongodb?
 yay -R dhcpcd
 yay -Rdd avahi v4l-utils
@@ -14,8 +13,9 @@ git config --global credential.helper store
 
 chmod a+x battery.sh
 sudo mv battery.sh /usr/local/bin/i3blocks/battery.sh
-sudo mkdir -p ~/.config/sway ~/.config/wofi ~/.config/termite ~/.config/mako ~/.config/i3blocks
-mv sway.conf ~/.config/sawy/config
+mkdir -p ~/.config/sway ~/.config/wofi ~/.config/termite ~/.config/mako ~/.config/i3blocks
+mkdir -p /usr/local/bin/i3blocks
+mv sway.conf ~/.config/sway/config
 mv wofi.conf ~/.config/wofi/config
 mv wofi.css ~/.config/wofi/style.css
 mv termite.conf ~/.config/termite/config
@@ -27,7 +27,6 @@ sudo mv /etc/lightdm/lightdm-mini-greeter.conf /etc/lightdm/lightdm-mini-greeter
 sudo mv lightdm-mini-greeter.conf /etc/lightdm/lightdm-mini-greeter.conf
 sudo mv /etc/mkinitcpio.conf /etc/mkinitcpio.old.conf
 sudo mv mkinitcpio.conf /etc/mkinitcpio.conf
-# sudodo mv /etc/default/grub /etc/default/grub.old
 sudo sed -i 's$GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"$GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet splash rd.udev.log_priority=3 vt.global_cursor_default=0"$' /etc/default/grub
 sudo mkinitcpio -p linux
 sudo grub-mkconfig -o /boot/grub/grub.cfg
