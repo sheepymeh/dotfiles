45fdec4a55339af09056b84f922a82f5485916f9
sheepymeh <sheepymeh@users.noreply.github.com>
2022-02-10 09:31:40 +0800

    need to mimic vscode nameLong for Pylance -.-

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -115,7 +115,8 @@ Set-Content -Path "$env:APPDATA\VSCodium\product.json" -Value @"
     "serviceUrl": "https://marketplace.visualstudio.com/_apis/public/gallery",
     "cacheUrl": "https://vscode.blob.core.windows.net/gallery/index",
     "itemUrl": "https://marketplace.visualstudio.com/items"
-  }
+  },
+  "nameLong": "Visual Studio Code"
 }
 "@
 
