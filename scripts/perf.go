2f3cc7954db016a00bdef8ebd746b7355e48ec68
sheepymeh <sheepymeh@users.noreply.github.com>
2022-12-07 22:48:19 +0800

    gestures, icons, and libreoffice

--- scripts/perf.go
+++ scripts/perf.go
@@ -12,13 +12,13 @@ func update(mode string) {
 	switch (mode) {
 		case "0x0":
 			ioutil.WriteFile("/proc/acpi/call", []byte("\\_SB.PCI0.LPC0.EC0.VPC0.DYTC 0x0012B001"), 0660)
-			fmt.Println(" Performance")
+			fmt.Println("")
 		case "0x1":
 			ioutil.WriteFile("/proc/acpi/call", []byte("\\_SB.PCI0.LPC0.EC0.VPC0.DYTC 0x0013B001"), 0660)
-			fmt.Println(" Battery")
+			fmt.Println("")
 		case "0x2":
 			ioutil.WriteFile("/proc/acpi/call", []byte("\\_SB.PCI0.LPC0.EC0.VPC0.DYTC 0x000FB001"), 0660)
-			fmt.Println(" Balanced")
+			fmt.Println("")
 		default:
 			help("Invalid mode")
 	}
